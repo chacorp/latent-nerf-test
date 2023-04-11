6d82691a5a31099509382ee6fa7f8f86dd75c680
chacorp <sihun.cha@gmail.com>
2023-04-11 14:33:58 +0000

    optimize text_embedding

--- src/stable_diffusion.py
+++ src/stable_diffusion.py
@@ -1,7 +1,22 @@
 from huggingface_hub import hf_hub_download
 from torchvision import transforms
-from transformers import CLIPTextModel, CLIPTokenizer, CLIPVisionModel,logging,CLIPProcessor
-from diffusers import AutoencoderKL, UNet2DConditionModel, PNDMScheduler
+from transformers import (
+    CLIPTextModel, 
+    CLIPTokenizer, 
+    CLIPVisionModel,
+    logging, 
+    CLIPProcessor, 
+    AutoProcessor,
+    CLIPTextModelWithProjection, 
+    CLIPVisionModelWithProjection,
+    CLIPModel,
+)
+from diffusers import (
+    AutoencoderKL, 
+    UNet2DModel,
+    UNet2DConditionModel, 
+    PNDMScheduler
+)
 
 # suppress partial model loading warning
 logging.set_verbosity_error()
@@ -10,11 +25,28 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from loguru import logger
-
+from PIL import Image
+from tqdm import tqdm
 import time
 
+
+# contrastive loss function, adapted from
+# https://sachinruk.github.io/blog/pytorch/pytorch%20lightning/loss%20function/gpu/2021/03/07/CLIP.html
+def contrastive_loss(logits: torch.Tensor) -> torch.Tensor:
+    return nn.functional.cross_entropy(logits, torch.arange(len(logits), dtype=torch.float16, device=logits.device))
+
+
+def clip_loss(similarity: torch.Tensor) -> torch.Tensor:
+    caption_loss = contrastive_loss(similarity)
+    image_loss = contrastive_loss(similarity.t())
+    return (caption_loss + image_loss) / 2.0
+
+
 class StableDiffusion(nn.Module):
-    def __init__(self, device, model_name='CompVis/stable-diffusion-v1-4',concept_name=None, latent_mode=True):
+    def __init__(self, device, 
+                 model_name='CompVis/stable-diffusion-v1-4',
+                 concept_name=None, 
+                 latent_mode=True):
         super().__init__()
 
         try:
@@ -37,12 +69,27 @@ class StableDiffusion(nn.Module):
         self.vae = AutoencoderKL.from_pretrained(model_name, subfolder="vae", use_auth_token=self.token).to(self.device)
 
         # 2. Load the tokenizer and text encoder to tokenize and encode the text. 
-        self.tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
-        self.text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14").to(self.device)
-        self.image_encoder = None
-        self.image_processor = None
-
-
+        self.model_id = "openai/clip-vit-large-patch14"
+        self.tokenizer = CLIPTokenizer.from_pretrained(self.model_id)
+        # self.text_encoder = CLIPTextModel.from_pretrained(self.model_id).to(self.device)
+        # self.image_encoder = None
+        # self.image_processor = None
+        
+        # self.text_encoder = CLIPTextModelWithProjection.from_pretrained(self.model_id).to(self.device)
+        # self.image_encoder = CLIPVisionModelWithProjection.from_pretrained(self.model_id).to(self.device)
+        self.image_processor = AutoProcessor.from_pretrained(self.model_id)
+        self.clipmodel       = CLIPModel.from_pretrained(self.model_id).to(self.device)
+        self.criterionCLIP   = torch.nn.CosineSimilarity(dim=1, eps=1e-15)
+        self.criterionL1     = torch.nn.L1Loss()
+        self.transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Resize((512, 512)),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ])
+        self.latent_gray = torch.tensor(
+                [0.9071, -0.7711,  0.7437,  0.1510]
+            )[None].unsqueeze(-1).unsqueeze(-1).to(self.device)
+        
         # 3. The UNet model for generating the latents.
         self.unet = UNet2DConditionModel.from_pretrained(model_name, subfolder="unet", use_auth_token=self.token).to(self.device)
 
@@ -87,24 +134,158 @@ class StableDiffusion(nn.Module):
 
     def get_text_embeds(self, prompt):
         # Tokenize text and get embeddings
-        text_input = self.tokenizer(prompt, padding='max_length', max_length=self.tokenizer.model_max_length, truncation=True, return_tensors='pt')
+        text_input = self.tokenizer(
+            prompt, 
+            padding='max_length', 
+            max_length=self.tokenizer.model_max_length, 
+            truncation=True, 
+            return_tensors='pt')
 
         with torch.no_grad():
-            text_embeddings = self.text_encoder(text_input.input_ids.to(self.device))[0]
+            # text_embeddings = self.text_encoder(text_input.input_ids.to(self.device))[0]
+            # text_embeddings = self.text_encoder(text_input.input_ids.to(self.device)).text_embeds
+            text_embeddings = self.clipmodel.text_model(text_input.input_ids.to(self.device))[0]
 
         # Do the same for unconditional embeddings
-        uncond_input = self.tokenizer([''] * len(prompt), padding='max_length', max_length=self.tokenizer.model_max_length, return_tensors='pt')
+        uncond_input = self.tokenizer(
+            [''] * len(prompt), 
+            padding='max_length', 
+            max_length=self.tokenizer.model_max_length, 
+            return_tensors='pt')
 
         with torch.no_grad():
-            uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(self.device))[0]
+            # uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(self.device))[0]
+            # uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(self.device)).text_embeds
+            uncond_embeddings = self.clipmodel.text_model(uncond_input.input_ids.to(self.device))[0]
 
         # Cat for final embeddings
         text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
         return text_embeddings
 
+    def get_image_embeds(self, image_path):
+        image = Image.open(image_path)
+        image_inputs = self.image_processor(
+                images=image, 
+                return_tensors="pt"
+            )        
+        with torch.no_grad():
+            image_features = self.clipmodel.get_image_features(image_inputs.pixel_values.to(self.device))
+            image_features = image_features / image_features.norm(p=2, dim=-1, keepdim=True)
+        return image_features, self.transform(image)
+    
+    def optimize_text_token(self, prompt, image_features, image, max_itr=100):
+        with torch.no_grad():
+            # txt -> token -> txt embed (hidden)
+            text_input = self.tokenizer(
+                prompt, 
+                padding='max_length', 
+                max_length=self.tokenizer.model_max_length, 
+                truncation=True, 
+                return_tensors='pt'
+            ).to(self.device)
+            # text_input = self.tokenizer(prompt, padding='max_length', max_length=self.tokenizer.model_max_length, truncation=True, return_tensors='pt').to(self.device)
+            text_embeddings = self.clipmodel.text_model(text_input.input_ids.to(self.device))[0]
+            
+            uncond_input = self.tokenizer(
+                [''] * len(prompt), 
+                padding='max_length', 
+                max_length=self.tokenizer.model_max_length, 
+                return_tensors='pt')
 
-    def train_step(self, text_embeddings, inputs, guidance_scale=100):
+            uncond_embeddings = self.clipmodel.text_model(uncond_input.input_ids.to(self.device))[0]
+            
+            ## normalized image feature
+            image_features = image_features / image_features.norm(p=2, dim=-1, keepdim=True)
+        text_embeddings.requires_grad = True
         
+        # image  = Image.open("/source/kseo/diffusion_playground/latent-nerf-test/data/monster_ball.jpg")
+        # prompt = "pokemon, monster ball, red and white color, on a grass"
+        # inputs = self.image_processor(text=[prompt], images=image, return_tensors="pt", padding=True)
+        # outputs = self.clipmodel(**inputs.to(self.device))
+        
+        itr = 0
+        save_itr = max_itr // 5
+        # max_itr = 100
+        pbar = tqdm(total=max_itr, initial=itr)
+        optimizer = torch.optim.Adam([text_embeddings], lr=1e-5, betas=(0.9, 0.99), eps=1e-15)
+        # scheduler = torch.optim.lr_scheduler.LambdaLR(
+        #     optimizer=optimizer,
+        #     lr_lambda=lambda epoch: 0.95 ** epoch,
+        #     last_epoch=-1,
+        #     verbose=False)
+
+        text_optimized = None
+        prev_loss = 100
+        while itr < max_itr:
+            itr += 1
+            pbar.update(1)
+            
+            optimizer.zero_grad()            
+            # txt embed (hidden) -> txt feature
+            pooled_output = text_embeddings[
+                torch.arange(text_embeddings.shape[0], device=self.device),
+                text_input.input_ids.to(dtype=torch.int, device=self.device).argmax(dim=-1),
+            ]
+            text_features = self.clipmodel.text_projection(pooled_output)[0]
+                
+            # normalized text features
+            text_features = text_features / text_features.norm(p=2, dim=-1, keepdim=True)
+
+            ## cosine similarity
+            loss = 1.0 - self.criterionCLIP(text_features, image_features.detach())
+            
+            loss.backward()
+            optimizer.step()
+            
+            # pbar.set_description("prev: {:06f} loss: {:06f} ".format(prev_loss, loss.item()))
+            pbar.set_description("loss: {:06f} ".format(loss.item()))
+            
+            if loss < prev_loss:
+                prev_loss      = loss.item()
+                text_optimized = text_embeddings.detach().clone()
+                
+            if itr % save_itr == 0 or itr == max_itr:
+                img = self.embeds_to_img(torch.cat([uncond_embeddings.detach(), text_embeddings]))                
+                Image.fromarray(img[0]).save('test_{:03}.png'.format(itr))
+                img = self.embeds_to_img(torch.cat([uncond_embeddings.detach(), text_optimized]))
+                Image.fromarray(img[0]).save('best.png')
+        return text_optimized
+
+    def embeds_to_img(self, text_embeddings, height=512, width=512, num_inference_steps=50, guidance_scale=7.5, latents=None):
+        # Text embeds -> img latents
+        # import pdb;pdb.set_trace()
+        text_embeddings.detach()
+        latents = self.produce_latents(text_embeddings, height=height, width=width, latents=latents, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale) # [1, 4, 64, 64]
+        
+        # Img latents -> imgs
+        imgs = self.decode_latents(latents) # [1, 3, 512, 512]
+
+        # Img to Numpy
+        imgs = imgs.detach().cpu().permute(0, 2, 3, 1).numpy()
+        imgs = (imgs * 255).round().astype('uint8')
+        
+        return imgs
+    
+    def embeds_to_img_tensor(self, text_embeddings, height=512, width=512, num_inference_steps=50, guidance_scale=7.5, latents=None):
+        # Text embeds -> img latents
+        text_embeddings.detach()
+        latents = self.produce_latents(text_embeddings, height=height, width=width, latents=latents, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale) # [1, 4, 64, 64]
+        
+        # Img latents -> imgs
+        imgs = self.decode_latents_grad(latents) # [1, 3, 512, 512]        
+        return imgs
+
+    def train_step(self, 
+                   text_embeddings, 
+                   image_embeddings, 
+                   inputs, 
+                   guidance_scale=100):
+        """
+        Input:
+            text_embeddings:    (n, 768), text feature after the projection
+            image_embeddings:   (n, 768), image feature after the projection
+            inputs:             (n,c,h,w), rendered latent image
+        """
         # interp to 512x512 to be fed into vae.
 
         # _t = time.time()
@@ -132,7 +313,11 @@ class StableDiffusion(nn.Module):
             latents_noisy = self.scheduler.add_noise(latents, noise, t)
             # pred noise
             latent_model_input = torch.cat([latents_noisy] * 2)
+            ### UNet2DModel,
+            # noise_pred = self.unet(latent_model_input, t).sample
+            ### UNet2DConditionModel, 
             noise_pred = self.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
+            
         # torch.cuda.synchronize(); print(f'[TIME] guiding: unet {time.time() - _t:.4f}s')
 
         # perform guidance (high scale from paper!)
@@ -189,10 +374,17 @@ class StableDiffusion(nn.Module):
         imgs = (imgs / 2 + 0.5).clamp(0, 1)
         
         return imgs
+    
+    def decode_latents_grad(self, latents):
+        # latents = F.interpolate(latents, (64, 64), mode='bilinear', align_corners=False)
+        latents = 1 / 0.18215 * latents
+
+        imgs = self.vae.decode(latents).sample
+        imgs = ((imgs * 0.5) + 0.5).clamp(0, 1)        
+        return imgs
 
     def encode_imgs(self, imgs):
         # imgs: [B, 3, H, W]
-
         imgs = 2 * imgs - 1
 
         posterior = self.vae.encode(imgs).latent_dist
